71230c55ca2b2fa2d9ea944dd5e919f973a84108
Minhyuk Jung <chmh0805@gmail.com>
2022-10-24 21:54:36 +0900

    6. Make To Do List

---
+++ js/todo.js
@@ -0,0 +1,54 @@
+const toDoForm = document.querySelector('form#todo-form')
+const toDoInput = toDoForm.querySelector('input')
+const toDoList = document.querySelector('ul#todo-list')
+
+const TODOS_KEY = 'todos'
+
+let toDos = []
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+}
+
+function deleteToDo(e) {
+  const li = e.target.parentElement
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id))
+  saveToDos()
+  li.remove()
+}
+
+function paintToDo(newToDo) {
+  const li = document.createElement('li')
+  li.id = newToDo.id
+  const span = document.createElement('span')
+  const button = document.createElement('button')
+  span.innerText = newToDo.text
+  button.innerText = '❌'
+  button.addEventListener('click', deleteToDo)
+  li.appendChild(span)
+  li.appendChild(button)
+  toDoList.appendChild(li)
+}
+
+function handleToDoSubmit(e) {
+  e.preventDefault()
+  const newToDo = toDoInput.value
+  toDoInput.value = ''
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  }
+  toDos.push(newToDoObj)
+  paintToDo(newToDoObj)
+  saveToDos()
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos)
+  toDos = parsedToDos
+  parsedToDos.forEach(paintToDo)
+}
